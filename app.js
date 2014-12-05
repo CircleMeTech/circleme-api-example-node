6fbdb907b828947013ac4ae4141426e8691ceebf
paolocorti <paolocorti87@gmail.com>
2014-12-05 12:56:32 +0100

    fix typo

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.session({
+app.use(session({
     cookie: {maxAge: 36000000},
     secret: 'circleme-test-app'
 }));
