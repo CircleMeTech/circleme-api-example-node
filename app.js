bb1ea4f54f8791516f1b4694696ee40e9856d9e0
paolocorti <paolocorti87@gmail.com>
2014-12-05 12:51:23 +0100

    add express-session module

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session')
 
 var routes = require('./routes/index');
 
@@ -22,6 +23,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.session({
+    cookie: {maxAge: 36000000},
+    secret: 'circleme-test-app'
+}));
 
 app.use('/', routes);
 
